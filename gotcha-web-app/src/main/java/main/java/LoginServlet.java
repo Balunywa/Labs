9ad2aabe29bbdbe05c8f41f0449da1a2174ed449
Lukman Balunywa <81341827+Balunywa@users.noreply.github.com>
2021-07-03 13:50:14 -0400

    updates
    made changes to the pom and updated the application port

--- gotcha-web-app/src/main/java/main/java/LoginServlet.java
+++ gotcha-web-app/src/main/java/main/java/LoginServlet.java
@@ -3,24 +3,20 @@ package main.java;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 
-		PrintWriter out = response.getWriter();
-		String Cx = "red";
-		if (Cx == "green") {
-			out.println("Hey Welocme to GotCha!!!");
-		} else {
-			out.println("Please enter the right user name");
-		}
+		String name = request.getParameter("name");
+        request.setAttribute("name", name);
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 	}
-
-	}
+}
